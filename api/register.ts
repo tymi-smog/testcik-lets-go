474ca2c4a7b9d254ff091e08f793c95cdeabfd34
tymi-smog <tymonsmigiel@gmail.com>
2026-02-20 13:22:45 +0100

    Update register.ts

--- api/register.ts
+++ api/register.ts
@@ -1,9 +1,9 @@
 console.log("API FILE LOADED");
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { sql } from "../lib/db.js";
+import { sql } from "./lib/db.js";
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
-import { resend } from "../lib/resend";
+import { resend } from "./lib/resend";
 
 export default async function handler(
   req: VercelRequest,
