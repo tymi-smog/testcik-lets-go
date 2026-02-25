3dc840aa341db34772afa75748716f902497eda4
tymi-smog <tymonsmigiel@gmail.com>
2026-02-25 10:51:22 +0100

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,6 +7,8 @@ import { Login } from "./pages/login";
 import { Register } from "./pages/register";
 import { Profile } from "./pages/profile";
 import { MyEvents } from "./pages/myevents";
+import { VerifyEmail } from "./pages/VerifyEmail";
+
 
 
 export const router = createBrowserRouter([
@@ -20,6 +22,9 @@ export const router = createBrowserRouter([
       { path: "login", Component: Login },
       { path: "register", Component: Register },
       { path: "profile", Component: Profile },
+      
+
+{ path: "verify-email", Component: VerifyEmail },
 { path: "my-events", Component: MyEvents },
 
     ],
