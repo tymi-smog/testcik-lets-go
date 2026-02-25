40f9ac5e9d7c26dbf2498891e4b4b25f35de3f9f
tymi-smog <tymonsmigiel@gmail.com>
2026-02-25 10:02:06 +0100

    Update register.ts

--- api/register.ts
+++ api/register.ts
@@ -23,6 +23,7 @@ export default async function handler(
         : req.body;
 
     const { username, email, password } = body;
+    console.log("REGISTER BODY:", body);
 
     if (!username || !email || !password) {
       return res.status(400).json({ error: "Missing fields" });
