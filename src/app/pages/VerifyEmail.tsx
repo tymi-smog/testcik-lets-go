4db2282bf9c3fb76f587f1b7d2212cb90b629306
tymi-smog <tymonsmigiel@gmail.com>
2026-02-25 10:50:30 +0100

    Create VerifyEmail.tsx

---
+++ src/app/pages/VerifyEmail.tsx
@@ -0,0 +1,12 @@
+export function VerifyEmail() {
+  return (
+    <div className="flex justify-center items-center min-h-screen text-center">
+      <div>
+        <h1 className="text-2xl font-semibold">Sprawdź swoją skrzynkę 📧</h1>
+        <p className="text-gray-500 mt-2">
+          Wysłaliśmy link aktywacyjny na Twój email.
+        </p>
+      </div>
+    </div>
+  );
+}
