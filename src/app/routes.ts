d6816b98cc3427d5dc2864ead8beeb48516d27fb
tymi-smog <tymonsmigiel@gmail.com>
2026-02-27 20:34:58 +0100

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,34 +1,39 @@
-import { createBrowserRouter } from 'react-router';
-import { Root } from './pages/Root';
-import { Home } from './pages/Home';
-import { EventDetail } from './pages/EventDetail';
-import { Checkout } from './pages/Checkout';
-import { Login } from "./pages/login";
-import { Register } from "./pages/register";
-import { Profile } from "./pages/profile";
-import { MyEvents } from "./pages/myevents";
+import { createBrowserRouter } from "react-router-dom";
+
+import { Root } from "./pages/Root";
+import { Home } from "./pages/Home";
+import { EventDetail } from "./pages/EventDetail";
+import { Checkout } from "./pages/Checkout";
+
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Profile } from "./pages/Profile";
+import { MyEvents } from "./pages/MyEvents";
+
 import { VerifyEmail } from "./pages/VerifyEmail";
+import { ForgotPassword } from "./pages/ForgotPassword";
 import { ResetPassword } from "./pages/ResetPassword";
-
-
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     Component: Root,
     children: [
       { index: true, Component: Home },
-      { path: 'event/:id', Component: EventDetail },
-      { path: 'checkout', Component: Checkout },
+
+      { path: "event/:id", Component: EventDetail },
+      { path: "checkout", Component: Checkout },
+
       { path: "login", Component: Login },
       { path: "register", Component: Register },
       { path: "profile", Component: Profile },
-      {path: "reset-password", Component: ResetPassword },
-      
+      { path: "my-events", Component: MyEvents },
 
-{ path: "verify-email", Component: VerifyEmail },
-{ path: "my-events", Component: MyEvents },
+      { path: "verify-email", Component: VerifyEmail },
 
+      // 🔐 Reset hasła flow
+      { path: "forgot-password", Component: ForgotPassword },
+      { path: "reset-password", Component: ResetPassword },
     ],
   },
 ]);
